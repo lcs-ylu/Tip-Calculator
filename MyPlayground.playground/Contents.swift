26ae4bfaa35fb134dc2fdefd52354be918b1fc36
Nancy Lu <ylu@lcs.on.ca>
2019-10-10 11:36:44 -0400

    launch screen

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let amountOfBill = 120.34
+let tipPercentageString = "15"
+let tipPercentageAsDouble = Double(tipPercentageString)
+let tipPercentage = 0.15
+let tip = amountOfBill * tipPercentage
+print(tip)
